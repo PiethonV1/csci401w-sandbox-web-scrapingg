63c01dceed995b08d4339da8d3c3e1cad5844894
DanySeth1989 <d.seth@osjl.com>
2023-10-28 17:38:03 -0400

    Update to Login

--- Source Code/Hinton_Login/hinton_website/hinton_website/urls.py
+++ Source Code/Hinton_Login/hinton_website/hinton_website/urls.py
@@ -1,11 +1,11 @@
+# URL's Hinton_website
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register/", v.register, name="register"),  # <-- added
+    path("register/", v.register, name="register"), 
     path('', include("main.urls")),
     path('', include("django.contrib.auth.urls")),
-    
 ]
--- Source Code/Hinton_Login/hinton_website/main/views.py
+++ Source Code/Hinton_Login/hinton_website/main/views.py
@@ -1,21 +1,29 @@
 import subprocess
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+def get_username(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+        return JsonResponse({"username": username})
+    else:
+        return JsonResponse({"error": "User is not authenticated"})
 
 def streamlit_view(request):
     user_name = request.user.username
 
     # Start the Streamlit app as a background process
     streamlit_command = [
-        'streamlit', 'run',
-        '/Users/danys/OneDrive/Documents/RIC/Fall 2023/CSCI 401W/csci401w-sandbox-web-scrapingg/Source Code/Hinton_Login/hinton_website/prediction.py'
-    ]
-
+    'streamlit', 'run',
+    '/Users/danys/OneDrive/Documents/RIC/Fall 2023/CSCI 401W/csci401w-sandbox-web-scrapingg/Source Code/Hinton_Login/hinton_website/prediction.py',
+    # Have streamlit open in streamlit_template without opening on a new tab
+    '--server.headless',
+    '--server.runOnSave',
+]
     subprocess.Popen(streamlit_command)
 
     return render(request, 'streamlit_template.html', {'user_name': user_name})
 
-
-
 def index(request):
-    return redirect('login')
+    return redirect('/login')
