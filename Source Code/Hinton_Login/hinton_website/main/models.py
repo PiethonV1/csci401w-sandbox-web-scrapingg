5697e88f10959f9f9758664e0bc05a3e0c30a4c7
DanySeth1989 <d.seth@osjl.com>
2023-11-20 18:47:22 -0500

    Testing Update

--- Source Code/Hinton_Login/hinton_website/main/models.py
+++ Source Code/Hinton_Login/hinton_website/main/models.py
@@ -2,11 +2,13 @@ from django.db import models
 # Testing DB creation by using Django Models
 # Create your models here.
 class StockData(models.Model):
+    volume = models.IntegerField()
     ticker = models.CharField(max_length=10)
     date = models.DateField()
     open = models.FloatField()
+    open_price = models.FloatField()
+    low = models.FloatField()
     close = models.FloatField()
-    testCol = models.CharField(max_length=20)
     
 def __str__(self):
     return f"{self.ticker} - {self.date}"
