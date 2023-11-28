420fe888b39008abea189dc203f001efcbfdc3cd
PiethonV1 <99272005+PiethonV1@users.noreply.github.com>
2023-11-27 20:37:44 -0500

    database_sqlite.py creation

--- Source Code/Hinton_Login/Scripts/database_sqlite.py
+++ Source Code/Hinton_Login/Scripts/database_sqlite.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Define the path to the directory and database
 db_directory = "../data"
-db_path = os.path.join(db_directory, 'users.db')
+db_path = os.path.join(db_directory, 'database.db')
 
 # Ensure the directory exists
 if not os.path.exists(db_directory):
@@ -17,74 +17,67 @@ cursor = conn.cursor()
 
 # Create the users table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT UNIQUE NOT NULL,
-        username TEXT UNIQUE NOT NULL,
-        date_joined DATE NOT NULL
-    )
+    CREATE TABLE IF NOT EXISTS Stock_Ticker(
+        Ticker VARCHAR (10),
+        Open_date DATE,
+        Open_Price DECIMAL(6,2),
+        High DECIMAL(6,2),
+        Low DECIMAL(6,2),
+        Close_price DECIMAL(6,2),
+        PRIMARY KEY (Ticker, Open_date)
+    )        
 ''')
 
 # Create the favorite_foods table
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS favorite_foods (
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        food_name TEXT NOT NULL,
-        date_added DATE NOT NULL,
-        rating INTEGER,
-        comments TEXT,
-        meal_time TEXT,
-        cuisine_type TEXT,
-        recipe_link TEXT,
-        FOREIGN KEY(user_id) REFERENCES users(id)
-    )
+    CREATE TABLEIF NOT EXISTS User(
+        UserName VARCHAR(20) UNIQUE NOT NULL, 
+        Email_Addr VARCHAR(100) CHECK (Email_Addr IN ('abcdefghijiklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ','~!@#$%_^&*()')),
+        PRIMARY KEY(UserName)
+    )           
 ''')
 
 # Insert 10 sample records for users
 
 # Insert 10 sample records
-sample_users = [
-    ("John Doe", "john@example.com", "john_doe", datetime.now().date()),
-    ("Jane Smith", "jane@example.com", "jane_smith", datetime.now().date()),
-    ("Alice", "alice@example.com", "alice", datetime.now().date()),
-    ("Bob", "bob@example.com", "bob", datetime.now().date()),
-    ("Charlie", "charlie@example.com", "charlie", datetime.now().date()),
-    ("David", "david@example.com", "david", datetime.now().date()),
-    ("Eve", "eve@example.com", "eve", datetime.now().date()),
-    ("Frank", "frank@example.com", "frank", datetime.now().date()),
-    ("Grace", "grace@example.com", "grace", datetime.now().date()),
-    ("Hannah", "hannah@example.com", "hannah", datetime.now().date())
+sample_user = [
+    ("Jason Foo", "jason@hinton.io"),
+    ("Joe Smith", "joe@example.com"),
+    ("Frank", "Frank@gmail.com"),
+    ("Bobby", "bob@eyahoo.com"),
+    ("Charlie", "charlie@hinton.io"),
+    ("Alice2", "alice@gmail.com"),
+    ("Eva ", "eve@example.com"),
+    ("Frank McAdams", "frank@emx.com"),
+    ("Grace Ma", "grace@custom.com"),
+    ("Benny C", "Benny@stopshop.com")
 ]
 
 cursor.executemany('''
-    INSERT OR IGNORE INTO users (name, email, username, date_joined) 
-    VALUES (?, ?, ?, ?)
-''', sample_users)
+    INSERT OR IGNORE INTO users (username, email) 
+    VALUES (?, ?)
+''', sample_user)
 
 # Insert sample records for favorite_foods for each user
-sample_foods = [
-    (1, "Pizza", datetime.now().date(), 5, "Love the cheese!", "Dinner", "Italian", "https://example.com/pizza_recipe"),
-    (1, "Burger", datetime.now().date(), 4, "Juicy and delicious!", "Lunch", "American", "https://example.com/burger_recipe"),
-    (1, "Ice Cream", datetime.now().date(), 4, "Favorite dessert!", "Dessert", "Dessert", "https://example.com/ice_cream_recipe"),
-    (2, "Tacos", datetime.now().date(), 4, "Always a delight!", "Lunch", "Mexican", "https://example.com/tacos_recipe"),
-    (2, "Sushi", datetime.now().date(), 5, "Fresh and tasty!", "Dinner", "Japanese", "https://example.com/sushi_recipe"),
-    (3, "Pasta", datetime.now().date(), 4, "Italian pasta!", "Dinner", "Italian", "https://example.com/pasta_recipe"),
-    (4, "Salad", datetime.now().date(), 3, "Healthy choice!", "Lunch", "Salad", "https://example.com/salad_recipe"),
-    (5, "Curry", datetime.now().date(), 4, "Spicy and flavorful!", "Dinner", "Indian", "https://example.com/curry_recipe"),
-    (6, "Steak", datetime.now().date(), 5, "Perfectly grilled!", "Dinner", "American", "https://example.com/steak_recipe"),
-    (7, "Sushi", datetime.now().date(), 4, "Fresh and delicious!", "Dinner", "Japanese", "https://example.com/sushi_recipe"),
-    (8, "Burger", datetime.now().date(), 4, "Classic burger!", "Lunch", "American", "https://example.com/burger_recipe"),
-    (9, "Pizza", datetime.now().date(), 5, "All-time favorite!", "Dinner", "Italian", "https://example.com/pizza_recipe"),
-    (10, "Ice Cream", datetime.now().date(), 5, "Sweet treat!", "Dessert", "Dessert", "https://example.com/ice_cream_recipe"),
+sample_Stock_Ticker = [
+    ("GOOG", 2023-11-20T00:00:00, 135.5000, 138.4250, 135.4900, 137.9200),
+    ("GOOG", 2023-11-21T00:00:00, 137.5000, 138.9650, 137.7050, 138.6200),
+    ("GOOG", 2023-11-22T00:00:00, 139.1000, 141.1000, 139.0000, 140.0200),
+    ("AAPL", 2023-11-20T00:00:00, 189.8900, 191.9100, 189.7400, 190.6400),
+    ("AAPL", 2023-11-21T00:00:00, 191.4100, 192.9300, 190.8300, 191.3100),
+    ("AAPL", 2023-11-22T00:00:00, 191.4900, 192.9300, 190.8300, 191.3100),
+    ("MSFT", 2023-11-20T00:00:00, 371.2200, 378.8700, 371.0000, 377.4400),
+    ("MSFT", 2023-11-21T00:00:00, 375.6700, 376.2200, 371.1200, 373.0700),
+    ("MSFT", 2023-11-22T00:00:00, 378.0000, 379.7900, 374.9700, 377.8500),
+    ("GME", 2023-11-20T00:00:00, 13.0000, 13.2000, 12.8000, 12.8000),
+    ("GME", 2023-11-21T00:00:00, 12.6500, 12.7200, 12.2600, 12.5500),
+    ("GME", 2023-11-22T00:00:00, 12.5600, 12.7100, 12.1900, 12.2900),
 ]
 
 cursor.executemany('''
-    INSERT INTO favorite_foods (user_id, food_name, date_added, rating, comments, meal_time, cuisine_type, recipe_link)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-''', sample_foods)
-
+    INSERT INTO Stock_Ticker (ticker, Open_date, Open_Price, High, Low, Close_price)
+    VALUES (?, datetime.now().date(), ?, ?, ?, ?)
+''', sample_Stock_Ticker)
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
